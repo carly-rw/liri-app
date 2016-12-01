12a5ff78c086af8fd12c4773b2d615932a5446f9
carlylumel <carlylucasmelanson@gmail.com>
2016-11-30 18:05:04 -0600

    Initial file creation.

---
+++ liri.js
@@ -0,0 +1,9 @@
+var twitterKeys = require("./keys.js");
+
+var TWconsumerKey = twitterKeys.consumer_key;
+var TWconsumerSecret = twitterKeys.consumer_secret;
+var TWaccessToken = twitterKeys.access_token_key;
+var TWaccessSecret = twitterKeys.access_token_secret;
+
+var input = process.argv[2];
+
